cf9aed7a95bbbe5267816e2fafb79fb8c469693f
Insath97 <insath1997.mi@gmail.com>
2025-02-06 01:46:39 +0530

    Fixed Working flow status

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Login &mdash; {{ getSettingInfo('site_name') }}</title>
 
+    <link rel="icon" href="{{ asset('admin/assets/img/favicon/favicon.ico') }}" type="image/x-icon">
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
